56f5dd90a1c9ed317dfe84c4892ab62efd8f7fc8
VEEPTY-SURESH <veepthisuresh@gmail.com>
2025-05-17 18:16:53 +0530

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,12 +14,20 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 builder.Services.AddCors();
 
+//AddScoped will effectively create a service avilable 
+//for entirety of the request
+builder.Services.AddTransient<ExceptionMiddleware>();
+
 var app = builder.Build();
 
 //configure the http request pipeline
+//order is important from here
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(opt =>
 {
-    _ = opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
 });
 app.MapControllers();
 
